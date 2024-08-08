d7e5ffef24e52410f8fcbf56207a5a8791ee9408
Fredy Pedro <38547323+Fredy12Pe@users.noreply.github.com>
2024-08-08 14:35:38 -0700

    changed timer to 4:30 and random gifs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function countdown() {
-    // Set the date and time to countdown to (5 PM today)
+    // Set the date and time to countdown to (4:30 PM today)
     const now = new Date();
     let countdownDate = new Date();
-    countdownDate.setHours(17, 0, 0, 0); // 5:00 PM
+    countdownDate.setHours(16, 30, 0, 0); // 4:30 PM
     
-    // If it's already past 5 PM, set the countdown for tomorrow's 5 PM
+    // If it's already past 4:30 PM, set the countdown for tomorrow's 4:30 PM
     if (now > countdownDate) {
         countdownDate.setDate(countdownDate.getDate() + 1);
     }
@@ -24,11 +24,49 @@ function countdown() {
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
-    // If the countdown is finished, write some text
+    // If the countdown is finished, show celebration GIFs
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "Time's up!";
+        showCelebrationGIFs();
     }
+}
+
+function showCelebrationGIFs() {
+    // Array of celebration GIF URLs
+    const gifURLs = [
+        'https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif',
+        'https://media.giphy.com/media/l41lVSY5H4MogTqeI/giphy.gif',
+        'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif',
+        'https://media.giphy.com/media/xT9IgIc0lryrxvqV6w/giphy.gif',
+        'https://media.giphy.com/media/3og0INyCmHlNylks9O/giphy.gif'
+    ];
+
+    // Container to hold the GIFs
+    const gifContainer = document.createElement('div');
+    gifContainer.style.position = 'fixed';
+    gifContainer.style.top = '0';
+    gifContainer.style.left = '0';
+    gifContainer.style.width = '100%';
+    gifContainer.style.height = '100%';
+    gifContainer.style.display = 'flex';
+    gifContainer.style.flexWrap = 'wrap';
+    gifContainer.style.justifyContent = 'center';
+    gifContainer.style.alignItems = 'center';
+    gifContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+
+    // Create an image element for each GIF and add to the container
+    gifURLs.forEach(url => {
+        const gif = document.createElement('img');
+        gif.src = url;
+        gif.style.width = '200px';
+        gif.style.margin = '10px';
+        gif.style.borderRadius = '10px';
+        gifContainer.appendChild(gif);
+    });
+
+    // Add the GIF container to the body
+    document.body.appendChild(gifContainer);
 }
 
 // Update the countdown every 1 second
